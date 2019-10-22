97974f3135c09dc396ed8f4032f1aa093eeecad1
Amila Welihinda <amilajack@gmail.com>
2019-10-22 04:40:09 -0700

    initial commit

---
+++ quicksort.py
@@ -0,0 +1,28 @@
+# Make all numpy available via shorter 'np' prefix
+import numpy as np
+# Make all matlib functions accessible at the top level via M.func()
+import numpy.matlib as M
+# Make some matlib functions accessible directly at the top level via, e.g. rand(3,3)
+from numpy.matlib import rand,zeros,ones,empty,eye
+# Define a Hermitian function
+def hermitian(A, **kwargs):
+    return np.transpose(A,**kwargs).conj()
+# Make some shortcuts for transpose,hermitian:
+#    np.transpose(A) --> T(A)
+#    hermitian(A) --> H(A)
+T = np.transpose
+H = hermitian
+
+print(H)
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+print(quicksort([3,6,8,10,1,2,1]))
+# Prints "[1, 1, 2, 3, 6, 8, 10]"
